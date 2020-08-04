f452f5ad0e95285e4c7f2507aecef89925114b7e
doron weiss <doron@10fs.co.il>
2020-08-04 11:53:41 +0300

    c# 8 ranges test fails

--- CS8Test.cs
+++ CS8Test.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+//using System.Range;
 
 namespace GenPurposeConsoleAppTest {
   enum Rainbow {
@@ -30,28 +31,45 @@ namespace GenPurposeConsoleAppTest {
       _ => throw new ArgumentException(message: "invalid enum value", paramName: nameof(colorBand)),
     };
 
-    static int Cmpr (int a, int b)=>
-      (a,b) switch{
-        (0,0) =>0,
-        (1,0) => 1,
-        (0,1)=>-1,
-        var(x,y) when x==y => 1000,
-        (_,_) =>-999
+    static int Cmpr(int a, int b) =>
+      (a, b) switch
+      {
+        (0, 0) => 0,
+        (1, 0) => 1,
+        (0, 1) => -1,
+        var (x, y) when x == y => 1000,
+        (_, _) => -999
       };
-    
+
     public static void Run() {
       Rainbow r = Rainbow.Orange;
       Console.WriteLine($"Color: {r} => {FromRainbow(r)}");
-      int a=1, b=0;
-      Console.WriteLine($"values: {a}/{b} => {Cmpr(a,b)}");
-      a = 0;b = 1;
-      Console.WriteLine($"values: {a}/{b} => {Cmpr(a,b)}");
+      int a = 1, b = 0;
+      Console.WriteLine($"values: {a}/{b} => {Cmpr(a, b)}");
+      a = 0; b = 1;
+      Console.WriteLine($"values: {a}/{b} => {Cmpr(a, b)}");
       a = 0; b = 0;
-      Console.WriteLine($"values: {a}/{b} => {Cmpr(a,b)}");
+      Console.WriteLine($"values: {a}/{b} => {Cmpr(a, b)}");
       a = 2; b = 2;
       Console.WriteLine($"values: {a}/{b} => {Cmpr(a, b)}");
       a = 2; b = 153;
       Console.WriteLine($"values: {a}/{b} => {Cmpr(a, b)}");
+      // ranges
+      double[] ds = { 1.0, 2, 3, 4, 5, 6, 7, 8, 9 };
+      var words = new string[]
+      {
+                    // index from start    index from end
+        "The",      // 0                   ^9
+        "quick",    // 1                   ^8
+        "brown",    // 2                   ^7
+        "fox",      // 3                   ^6
+        "jumped",   // 4                   ^5
+        "over",     // 5                   ^4
+        "the",      // 6                   ^3
+        "lazy",     // 7                   ^2
+        "dog"       // 8                   ^1
+      };
+      //Console.WriteLine($"The last word is {words[^1]}");
     }
   }
 }
