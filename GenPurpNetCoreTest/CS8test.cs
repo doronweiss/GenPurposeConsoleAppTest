2f9c65d75144f063b1fde62fe7a5c0f898ff53cc
doron weiss <doron@10fs.co.il>
2020-08-04 12:01:28 +0300

    added net core test

---
+++ GenPurpNetCoreTest/CS8test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenPurpNetCoreTest {
+  class CS8test {
+    public static void
+      Run() {
+      var words = new string[]
+      {
+                    // index from start    index from end
+        "The",      // 0                   ^9
+        "quick",    // 1                   ^8
+        "brown",    // 2                   ^7
+        "fox",      // 3                   ^6
+        "jumped",   // 4                   ^5
+        "over",     // 5                   ^4
+        "the",      // 6                   ^3
+        "lazy",     // 7                   ^2
+        "dog"       // 8                   ^1
+      };
+      Console.WriteLine($"The last word is {words[^1]}");
+      Console.WriteLine($"Range length is {words[2..3].Length}");
+      var v = words[2..6];
+      foreach (string s in v)
+        Console.WriteLine($"Word = {s}");
+    }
+  }
+}
