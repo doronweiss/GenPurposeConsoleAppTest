1c99d8eb535ac09534a751e908bff923286397dd
doron <doron@10fs.co.il>
2020-11-16 20:17:28 +0200

    "unchecked" test

---
+++ GenPurpNetCoreTest/GeneralPurposeTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenPurpNetCoreTest {
+  class GeneralPurposeTests {
+    public static void Run() {
+      const int hardwareOutput = 0x8000;
+      short sh = 0;
+      unchecked {
+        sh |= (short)hardwareOutput;
+      }
+      Console.WriteLine($"sh = {sh}");
+    }
+  }
+}
