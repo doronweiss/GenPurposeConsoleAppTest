aa488838ab0acad7ccae7ed01d87d2cba93c47ac
doron <doron@10fs.co.il>
2021-03-30 19:47:05 +0300

    added NetworkIPChange.cs

---
+++ GenPurpNetCoreTest/NetworkIPChange.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenPurpNetCoreTest {
+
+  class NetworkIPChange {
+
+    public static NetworkInterface FindlLocalLan1EthernetID(string intrfcName) {
+      string res = "";
+      try {
+        System.Net.NetworkInformation.NetworkInterface[] networkCards
+          = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces();
+        foreach (NetworkInterface intrfc in networkCards) {
+          if (intrfc.Name == intrfcName) {
+            return intrfc;
+          }
+        }
+      } catch (Exception ex) {
+        res = "";
+      }
+      return null;
+    }
+
+    public static void Run() {
+      NetworkInterface ni = FindlLocalLan1EthernetID("Ethernet");
+      if (ni != null) { 
+        Console.WriteLine(ni.Description);
+        Console.WriteLine(ni.Id);
+        Console.WriteLine(ni.NetworkInterfaceType);
+        Console.WriteLine(ni.GetPhysicalAddress().ToString());
+        IPInterfaceProperties properties = ni.GetIPProperties();
+      }
+    }
+
+  }
+}
--- GenPurpNetCoreTest/Program.cs
+++ GenPurpNetCoreTest/Program.cs
@@ -4,12 +4,13 @@ namespace GenPurpNetCoreTest {
   class Program {
     static void Main(string[] args) {
       //CS8test.Run();
-      JSONtest.Run();
+      //JSONtest.Run();
       //NestedClassTest.Run();
       //AnonJsonTest.Run();
       //PM8ConfigEncDec.Run();
       //AttributesTest.Run();
       //DupTestClass.Run();
+      NetworkIPChange.Run();
     }
   }
 }
