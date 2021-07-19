d7292ad363eb62584fa3076b506b8c1c1cb96e2e
doron weiss <doron@10fs.co.il>
2021-07-19 18:44:14 +0300

    relocated matlib

---
+++ MatLib/DMatrix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatLib {
+  class DMatrix<T> {
+    // actual data
+    private T[,] mat;
+    private readonly int rows, cols;
+    // constructors
+    public DMatrix(int rows, int cols) {
+      this.rows = rows;
+      this.cols = cols;
+      this.mat = new T[rows, cols];
+    }
+    public DMatrix (DMatrix<T> src){
+      this.rows = src.rows;
+      this.cols = src.cols;
+      this.mat = new T[rows, cols];
+      for (int idx=0; idx<rows;idx++)
+      for (int jdx = 0; jdx < cols; jdx++)
+        mat[idx, jdx] = src.mat[idx, jdx];
+    }
+    // accessor
+    public T this[int idx, int jdx] {
+      get => mat[idx, jdx];
+      set => mat[idx, jdx] = value;
+    }
+  }
+}
---
+++ MatLib/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MatLib {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
