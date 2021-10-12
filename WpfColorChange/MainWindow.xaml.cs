ad411f87ffa3b2bef5f79d5229c3b277bc6c722a
doron weiss <doron@10fs.co.il>
2021-10-12 16:24:56 +0300

    color image

---
+++ WpfColorChange/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using PixelFormat = System.Windows.Media.PixelFormat;
+using Rectangle = System.Windows.Shapes.Rectangle;
+
+namespace WpfColorChange {
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window {
+    public MainWindow() {
+      InitializeComponent();
+    }
+
+    private void OnLoadImage(object sender, RoutedEventArgs e) {
+      BitmapImage bitmap = new BitmapImage();
+      bitmap.BeginInit();
+      bitmap.UriSource = new Uri(@"c:\temp\blacksource.png");
+      bitmap.EndInit();
+      img.Source = bitmap;
+    }
+
+  }
+}
