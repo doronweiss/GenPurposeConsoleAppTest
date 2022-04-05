b3ca2c3c88bfb9cd3824c023b83bb30e7ad42c71
doron weiss <doron@10fs.co.il>
2022-04-05 14:06:48 +0300

    Added SE project

---
+++ SEUtils/LogTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SEUtils {
+  internal class LogTest {
+    public static void Run() {
+      var father = new {Logtype = "new order", id = "621232ff4adff200045de5e1", table = 1, burgersNum = 3, burgers = new List<object>()};
+      father.burgers.Add(new {
+        Sbid = "WAITER", Type = "Sporty", Size = "Reg"
+      });
+      father.burgers.Add(new {
+        Sbid = "WAITER2", Type = "Sporty", Size = "Reg"
+      });
+      father.burgers.Add(new {
+        Sbid = "WAITER3", Type = "Sporty", Size = "Reg"
+      });
+      Console.WriteLine(JsonConvert.SerializeObject(father));
+    }
+  }
+}
---
+++ SEUtils/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SEUtils {
+  internal class Program {
+    static void Main(string[] args) {
+      LogTest.Run();
+    }
+  }
+}
