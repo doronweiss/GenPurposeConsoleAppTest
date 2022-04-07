d603f2e00990805005006b5d6fc5a82fad863360
doron weiss <doron@10fs.co.il>
2022-04-07 16:40:32 +0300

    bu

--- Gorillas/Base64Test.cs
+++ Gorillas/Base64Test.cs
@@ -7,29 +7,25 @@ using Newtonsoft.Json;
 
 namespace Gorillas {
   internal class Base64Test {
-    static char[] HexChars = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
     public static string Bytes2HexString(byte[] data) {
       StringBuilder sb = new StringBuilder();
-      foreach (byte bt in data) {
-        int low = bt & 0xf;
-        int high = (bt & 0xf0) >> 4;
-        sb.Append(HexChars[low]);
-        sb.Append(HexChars[high]);
-      }
+      data.ToList().ForEach(x => sb.Append(x.ToString("X2")));
       return sb.ToString();
     }
     public static void Run() {
-      string str = "AAAAAAAAAAAAEKrYeyUnT6gX+R/BsW8=";
+      string str = "AAAAAAAAAAAA7O/nKeRiHcyRtUAI/oM=";
       byte[] data = Convert.FromBase64String(str);
       Console.WriteLine("Req:");
       Console.WriteLine($"Total of {data.Length} bytes");
       Console.WriteLine(Bytes2HexString(data));
+      System.Diagnostics.Debug.WriteLine(Bytes2HexString(data));
       str = "ICO2BUTZ8xCbUf11nr8j5puakdu4lgHSKk9CXsYtst3R";
       data = Convert.FromBase64String(str);
       Console.WriteLine("Resp:");
       Console.WriteLine($"Total of {data.Length} bytes");
       Console.WriteLine(Bytes2HexString(data));
+      System.Diagnostics.Debug.WriteLine(Bytes2HexString(data));
     }
   }
 }
