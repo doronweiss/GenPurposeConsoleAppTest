78b33f5a35a0d7a8ccce1fee7a2682fab4b0e9d6
doron weiss <doron@10fs.co.il>
2022-07-18 15:47:50 +0300

    added airtiuch project

---
+++ Airtouch/GenTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airtouch {
+  internal class GenTests {
+    public static void Run() { 
+      int i = Int32.MinValue;
+      Console.WriteLine($"int: {i}");
+      uint ui = BitConverter.ToUInt32(BitConverter.GetBytes(i), 0);
+      Console.WriteLine($"uint: {ui}");
+    }
+  }
+}
