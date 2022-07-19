0f310302e607cc10acef1b0d4e377730b1dd96e2
doron weiss <doron@10fs.co.il>
2022-07-19 13:00:18 +0300

    handling json with null values

---
+++ Airtouch/PartialJson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Airtouch {
+  internal class PartialJson {
+    class C2bJasoned {
+      [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+      public int? iValue;
+      public int? iValue2;
+      [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+      public string str = "batata";
+    }
+
+    public static void Run() {
+      C2bJasoned c2b = new C2bJasoned();
+      Console.WriteLine(JsonConvert.SerializeObject(c2b, Formatting.Indented));
+      c2b.str = null;
+      Console.WriteLine(JsonConvert.SerializeObject(c2b, Formatting.Indented));
+    }
+  }
+}
