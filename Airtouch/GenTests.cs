cd4b4663344756378d28dcf8cf46851345619703
doron weiss <doron@10fs.co.il>
2022-08-04 14:22:46 +0300

    tested records and "with" keyword

--- Airtouch/GenTests.cs
+++ Airtouch/GenTests.cs
@@ -6,11 +6,25 @@ using System.Threading.Tasks;
 
 namespace Airtouch {
   internal class GenTests {
+
+    record class DemoCls {
+      public int i;
+      public int j;
+
+      public override string ToString () =>
+        $"Demo class, i: {i}, j: {j}";
+    }
+
     public static void Run() { 
       int i = Int32.MinValue;
       Console.WriteLine($"int: {i}");
       uint ui = BitConverter.ToUInt32(BitConverter.GetBytes(i), 0);
       Console.WriteLine($"uint: {ui}");
+      //
+      DemoCls dc1 = new DemoCls() {i = 2, j = 3};
+      Console.WriteLine($"dc1: {dc1}");
+      DemoCls dc2 = dc1 with {j = 4};
+      Console.WriteLine($"dc1: {dc2}");
     }
   }
 }
