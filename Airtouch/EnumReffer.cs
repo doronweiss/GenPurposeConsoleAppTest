d8aca2a113e6e1ac484fbb86634a151a5062baf3
doron <doron@10fs.co.il>
2022-10-04 15:52:37 +0300

    BU

---
+++ Airtouch/EnumReffer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://stackoverflow.com/questions/2256048/store-a-reference-to-a-value-type
+
+namespace Airtouch {
+  internal class EnumReffer {
+    enum AnEnum {
+      A,
+      B,
+      C
+    };
+
+    class TgtClass {
+      public AnEnum en;
+    }
+
+    class EnRefferal<T>  { 
+      T locRef;
+
+      // public EnRefferal(T remote) {
+      //   locRef = &remote;
+      // }
+    }
+
+
+    public static void Run() { }
+  }
+}
