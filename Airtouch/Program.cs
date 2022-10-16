75edad669c5a63d8388d3171a8609ccad4123a84
doron <doron@10fs.co.il>
2022-10-16 18:50:11 +0300

    added record test

--- Airtouch/Program.cs
+++ Airtouch/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-Airtouch.GenTests.Run();
+
+//Airtouch.GenTests.Run();
 //Airtouch.PartialJson.Run();
+Airtouch.RecordTest.Run();
---
+++ Airtouch/RecordTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airtouch {
+  record MyRec(string name, int age);
+
+  internal class RecordTest {
+    public static void Run() {
+      var mr1 = new MyRec("doron", 62);
+      Console.WriteLine($"mr1= {mr1}");
+      var mr2 =  mr1 with{age = 50};
+      Console.WriteLine($"mr2= {mr2}");
+    }
+  }
+}
