93f59ed84fb98241ef81eb79996a54d38e762e12
doron <doron@10fs.co.il>
2023-01-24 19:49:53 +0200

    added PLCMultiBool class

--- UtilsClassesTests/ChangeDetector.cs
+++ UtilsClassesTests/ChangeDetector.cs
@@ -26,4 +26,32 @@ namespace UtilsClassesTests {
     public static implicit operator bool(PLCMultiBool16 b) => b.bv.Data!=0;
 
   }
+
+  internal class PLCMultiBool {
+    private int dataSize = 16;
+    private List<PLCBool> plcBools = default;
+
+    public PLCMultiBool(int dataSize, bool initValue) {
+      this.dataSize = dataSize;
+      plcBools = new List<PLCBool>();
+      for (int idx = 0; idx < dataSize; idx++)
+        plcBools.Add(new PLCBool(initValue));
+    }
+
+    public PLCBoolValues Set(int idx, bool value) {
+      if (idx < 0 || idx >= dataSize)
+        return PLCBoolValues.False;
+      return plcBools[idx].Set(value);
+    }
+
+    public void Reset() => 
+      plcBools.ForEach(x => x.Reset());
+
+    public bool IsRise => plcBools.Any(x => x.IsRise);
+
+    public bool IsFall => plcBools.Any(x => x.IsFall);
+
+    public bool Changed => IsRise || IsFall;
+
+  }
 }
