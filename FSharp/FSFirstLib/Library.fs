ad016df594161d5f513e1c797020f078077d1825
doron <doron@10fs.co.il>
2023-05-18 20:38:38 +0300

    use F# type in C#

--- FSharp/FSFirstLib/Library.fs
+++ FSharp/FSFirstLib/Library.fs
@@ -1,7 +1,22 @@
 ﻿namespace FSFirstLib
+open System
 open ServiceStack.OrmLite.Sqlite
 
 module FSFirstLib =
+    type Person(firstName : string, lastName : string) = 
+    /// The fullName of the Person, computed when the object is constructed
+      let fullName = String.Format("{0} {1}",firstName, lastName)
+      //2nd constructor
+      new() = Person("", "")
+      //3rd constructor
+      new(firstName : string) = Person(firstName, "")
+      // 'this' specifies a name for the object's self identifier.
+      // In instance methods, it must appear before the member name.
+      member public this.FirstName = firstName
+      member public this.LastName = lastName
+      member public this.FullName = fullName
+
+
     let MyMax a b =
         if a >= b  then a else b
 
--- FSharp/TesterApp/Program.cs
+++ FSharp/TesterApp/Program.cs
@@ -5,29 +5,32 @@ using Microsoft.FSharp.Collections;
 static int MySum2(List<int> lst) => lst[0] + lst[1];
 
 // // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-double d = FSFirstLib.FSFirstLib.MyMax(1.2, 3.4);
-Console.WriteLine($"Max = {d}");
+//Console.WriteLine("Hello, World!");
+//double d = FSFirstLib.FSFirstLib.MyMax(1.2, 3.4);
+//Console.WriteLine($"Max = {d}");
 
-List<int> lst = new List<int>() { 1, 2, 3, 4, 5 };
- Console.WriteLine($"lst sum =  {FSFirstLib.FSFirstLib.SumF2Seq(lst)}");
+//List<int> lst = new List<int>() { 1, 2, 3, 4, 5 };
+// Console.WriteLine($"lst sum =  {FSFirstLib.FSFirstLib.SumF2Seq(lst)}");
 
-int loops = 1000000, sum=0;
-Stopwatch st = new Stopwatch();
-Console.WriteLine("Starting");
-st.Start();
- for (int idx = 0; idx < loops; idx++)
-  sum = FSFirstLib.FSFirstLib.SumF2Seq(lst);
-st.Stop();
-long fpt = st.ElapsedMilliseconds;
-Console.WriteLine($"SEQ:   Elapssed: {fpt}, per op: {fpt * 1.0 / loops}m sum: {sum}");
-st.Reset();
-st.Start();
- for (int idx = 0; idx < loops; idx++)
-  sum = FSFirstLib.FSFirstLib.SumF2(ListModule.OfSeq(lst));
-st.Stop();
-fpt = st.ElapsedMilliseconds;
-Console.WriteLine($"LIST:   Elapssed: {fpt}, per op: {fpt * 1.0 / loops}m sum: {sum}");
+//int loops = 1000000, sum=0;
+//Stopwatch st = new Stopwatch();
+//Console.WriteLine("Starting");
+//st.Start();
+// for (int idx = 0; idx < loops; idx++)
+//  sum = FSFirstLib.FSFirstLib.SumF2Seq(lst);
+//st.Stop();
+//long fpt = st.ElapsedMilliseconds;
+//Console.WriteLine($"SEQ:   Elapssed: {fpt}, per op: {fpt * 1.0 / loops}m sum: {sum}");
+//st.Reset();
+//st.Start();
+// for (int idx = 0; idx < loops; idx++)
+//  sum = FSFirstLib.FSFirstLib.SumF2(ListModule.OfSeq(lst));
+//st.Stop();
+//fpt = st.ElapsedMilliseconds;
+//Console.WriteLine($"LIST:   Elapssed: {fpt}, per op: {fpt * 1.0 / loops}m sum: {sum}");
+
+FSFirstLib.FSFirstLib.Person pers = new FSFirstLib.FSFirstLib.Person("moshe", "levi");
+Console.WriteLine(pers.FullName);
 
 
 
