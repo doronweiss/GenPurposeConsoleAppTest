54225d3c199dc8a6f0d8c7a05aea7389a65be292
doron <doron@10fs.co.il>
2023-12-22 18:02:15 +0200

    added first avalonia UI project

---
+++ Avalonia/FirstTest/Views/MainWindow.axaml.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace FirstTest.Views;
+
+public partial class MainWindow : Window {
+  private int idx = 0;
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OnClick(object? sender, RoutedEventArgs e) {
+      outputTBLK.Text = $"Clicked {++idx} times ....";
+    }
+}
