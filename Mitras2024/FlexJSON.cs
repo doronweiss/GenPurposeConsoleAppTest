e0b711858f307ba3c742fe8ed6566600dc2bea01
doron <doron@10fs.co.il>
2024-05-17 14:58:44 +0300

    added mitras2024 project and FlexJSON test many changes due to nugets update

---
+++ Mitras2024/FlexJSON.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Mitras2024 {
+  internal class FlexJSON {
+    public static void Run() {
+      string json = File.ReadAllText("c:\\Users\\User\\OneDrive\\Projects\\Aerodan\\mitrasdata.json");
+      Newtonsoft.Json.Linq.JObject jsonObj = JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(json);
+      Console.WriteLine(jsonObj["vehicle"]["capabilities"]);
+    }
+  }
+}
---
+++ Mitras2024/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Mitras2024 {
+  internal class Program {
+    static void Main(string[] args) {
+      FlexJSON.Run();
+    }
+  }
+}
