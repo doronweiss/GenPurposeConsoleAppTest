984d2fe89dea095076afc4866fe2b5de0b052305
doron <doron@10fs.co.il>
2024-06-29 19:38:13 +0300

    added  iard + iard/PartialJSONTest

---
+++ Iard/PartialJSONTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Iard {
+  public class BoundingBoxEx {
+    public List<int> bb { get; set; }
+    public double confidence { get; set; }
+    public string label { get; set; }
+  }
+
+  public class Tiles {
+    public List<List<int>> grid { get; set; }
+    public List<BoundingBoxEx> bb { get; set; }
+  }
+
+  public class Merged {
+    public List<BoundingBoxEx> bb { get; set; }
+  }
+
+  internal class PartialJSONTest {
+    public static void Run() {
+      Merged merged = new Merged();
+      merged.bb = new List<BoundingBoxEx>() { new BoundingBoxEx() { bb = new List<int>() { 1, 2, 3, 4 }, confidence = 0.5, label = "sdsds" } };
+      string json = Newtonsoft.Json.JsonConvert.SerializeObject(merged);
+      Tiles tls = JsonConvert.DeserializeObject<Tiles>(json);
+      Console.WriteLine(JsonConvert.SerializeObject(tls));
+    }
+  }
+}
