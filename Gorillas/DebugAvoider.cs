da05f17d0e96f9cc9511d41bd1d2b9baa147e35a
doron <doron@10fs.co.il>
2024-07-28 19:58:13 +0300

    gorilla - prevent debugging

---
+++ Gorillas/DebugAvoider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://dunnhq.com/posts/2022/debugger-attributes/
+
+namespace Gorillas {
+  internal class DebugAvoider {
+    //[DebuggerStepThrough()]
+    [DebuggerHidden]
+    private static void Write2Lines() {
+      Console.WriteLine("Line 2");
+      Console.WriteLine("Line 3");
+    }
+
+    public static void Run() { 
+      Console.WriteLine("Line 1");
+      Write2Lines();
+      Console.WriteLine("Line 4");
+    }
+  }
+}
--- Gorillas/Program.cs
+++ Gorillas/Program.cs
@@ -5,7 +5,8 @@ namespace Gorillas {
     static void Main(string[] args) {
       //Base64Test.Run();
       //MICCalculator.Run();
-      OrbCommRegex.Run();
+      //OrbCommRegex.Run();
+      DebugAvoider.Run();
     }
   }
 }
